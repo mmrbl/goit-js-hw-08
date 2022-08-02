1747d5d11dc0ea5b45ddd4e2a6d2d27e39542d87
Ihor <itotonk1@gmail.com>
2022-08-02 23:03:26 +0300

    Додає першу таску

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+//деструктуризація об'єкта galleryItems і додавання необхідних атрибутів у розмітку, яка буде створюватися
+
+const createGallery = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', createGallery);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
