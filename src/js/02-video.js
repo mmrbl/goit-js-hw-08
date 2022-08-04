6e02939dffd22e898d5a61dcba0d5234fb81f835
Ihor <itotonk1@gmail.com>
2022-08-04 18:13:22 +0300

    Додає другу таску

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+//записую в локальне сховище поточний час. throttle встановлює крок в 1 секунду
+const onPlay = throttle(function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+//розпарсив час, який записався у стрічках 9-10
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+//
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', onPlay);
